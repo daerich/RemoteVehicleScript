14a51d0f2f39dce1e2fb30a63e313d76bbe7b1d7
DaErich <fakefakefans@gmail.com>
2020-05-17 00:22:49 +0200

    inital commit

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rage;
+using Remote_Vehicle_Locker.Functions;
+
+[assembly: Rage.Attributes.Plugin("Remote Vehicle Locker", Description = "Remotely Locks your Vehicle", Author = "DaErich")]
+
+
+namespace Remote_Vehicle_Locker
+{
+    public static class EntryPoint
+    {
+        
+        private static void Main()
+        {
+            Plugin Plg = new Plugin();
+            
+            GameFiber.StartNew(Plg.KeyEvent);
+        }
+      
+    }
+    
+    
+}
+
+
---
+++ Plugin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rage;
+using Remote_Vehicle_Locker;
+
+namespace Remote_Vehicle_Locker.Functions
+{
+    internal class Plugin
+    {
+        private static Player player = Game.LocalPlayer;
+        private Vehicle myVehicle = player.LastVehicle;
+        internal void KeyEvent()
+        {
+            KeyboardState Keyboard = new KeyboardState();
+
+            if (Keyboard.PressedKeys.Contains(Keys.L))
+            {
+                CloseVehicleDoors(myVehicle);
+                LockCar(myVehicle);
+                BlipSiren(myVehicle);
+                GameFiber.ExecuteFor(FlashIndy, 3000);
+
+            }
+        }
+
+        private void FlashIndy()
+        {
+            myVehicle.IndicatorLightsStatus = VehicleIndicatorLightsStatus.Both;
+        }
+        private static void BlipSiren(Vehicle Vehicle)
+        {
+            if (Vehicle.HasSiren)
+            {
+                Vehicle.BlipSiren(false);
+            }
+
+
+        }
+        private static void LockCar(Vehicle Vehicle)
+        {
+            Vehicle.LockStatus = VehicleLockStatus.Locked;
+        }
+        private static void CloseVehicleDoors(Vehicle Vehicle)
+        {
+            VehicleDoor[] vDoor = Vehicle.GetDoors();
+
+            foreach (VehicleDoor door in vDoor)
+            {
+                if (door.IsOpen && door.IsValid())
+                {
+                    door.Close(true);
+                }
+            }
+
+        }
+    }
+
+}
