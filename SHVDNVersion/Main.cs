b0aedffa1abdb127cde40dd2c5727ed8728c0f21
DaErich <fakefakefans@gmail.com>
2020-05-27 15:12:07 +0200

    added shvdn version

---
+++ SHVDNVersion/Main.cs
@@ -0,0 +1,43 @@
+﻿using GTA;
+using RMVL_Scripthookv.Functions;
+using DaErich.Core;
+using System.Windows.Forms;
+using System;
+
+namespace RMVL_Scripthookv
+{
+    class RemoteVehicleLocker : Script
+    {
+        public RemoteVehicleLocker()
+        {
+            //Tick += OnTick;
+            KeyDown += OnKeyDown;
+        }
+
+        /*  private void OnTick(object sender, EventArgs e)
+          {
+
+          } */
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            const string cvars = "LockKey";
+            FileReader FileRD = new FileReader("scripts/VehicleLocker.ini", cvars);
+            Enum.TryParse(FileRD.GetCurrentValue(), out Keys kresult);
+            if (kresult == e.KeyCode)
+            {
+                Plugin Plg = new Plugin();
+                Vehicle myVehicle = Plg.Vehicle;
+                if (myVehicle != null && Plugin.Player.LastVehicle.Position.DistanceTo(Plugin.Player.Character.Position) <= 100f)
+                {
+                    //Game.LogTrivial("Key Pressed");
+                    Plg.BlipSiren();
+                    Plg.FlashIndy();
+                    Plg.LockCar();
+                    Plg.CloseVehicleDoors();
+                    Wait(2000);
+                }
+            }
+        }
+    }
+}
---
+++ SHVDNVersion/Plugin.cs
@@ -0,0 +1,84 @@
+﻿using GTA;
+using GTA.UI;
+
+namespace RMVL_Scripthookv.Functions
+{
+   internal class Plugin
+    {
+        internal static Player Player = Game.Player;
+        internal Vehicle Vehicle { get; set; } //do not declare public instance fields!
+
+        public Plugin()
+        {
+            Vehicle = Player.Character.LastVehicle;
+        }
+
+        internal void FlashIndy()
+        {
+
+            Vehicle.IsLeftIndicatorLightOn = true;
+            Vehicle.IsRightIndicatorLightOn = true;
+            Script.Wait(500);
+            Vehicle.IsLeftIndicatorLightOn = false;
+            Vehicle.IsRightIndicatorLightOn = false;
+        }
+        internal void BlipSiren()
+        {
+            Vehicle.SoundHorn(300);
+            /*if (Vehicle.HasSiren)
+            {
+                if (Vehicle.IsSirenActive)
+                {
+                    Vehicle.IsSirenSilent = false;
+                    Script.Wait(500);
+                    Vehicle.IsSirenSilent = true;
+                }
+
+
+                else
+                {
+                   
+                }
+
+            }*/
+
+
+        }
+        internal void LockCar()
+        {
+            // if (Vehicle.Driver == null)
+            //{                                     Funny little artifact
+            if (Vehicle.LockStatus == VehicleLockStatus.Locked)
+            {
+                int PINSUP = Notification.Show("*Pins UP*", true);
+                Vehicle.LockStatus = VehicleLockStatus.Unlocked;
+                Script.Wait(1000);
+                Notification.Hide(PINSUP);
+                
+            }
+
+
+
+            else
+            {
+                Vehicle.LockStatus = VehicleLockStatus.Locked;
+               int PINSD = Notification.Show("*Pins DOWN*", true);
+                Script.Wait(1000);
+                Notification.Hide(PINSD);
+            }
+        }
+        internal void CloseVehicleDoors()
+        {
+            VehicleDoorCollection vDoor = Vehicle.Doors;
+
+            foreach (VehicleDoor door in vDoor.ToArray())
+            {
+                if (door.IsOpen)
+                {
+                    door.Close(false);
+                }
+            }
+
+        }
+    }
+}
